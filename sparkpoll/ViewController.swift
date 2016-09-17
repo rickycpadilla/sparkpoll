03bfd3e912e9c5ceea0c64abd38591687e983107
Alexander Murphy <amurphy345@gmail.com>
2016-09-17 10:13:08 -0600

    adding google services plist and importing firebase to view controller

--- sparkpoll/ViewController.swift
+++ sparkpoll/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    let firebaseRoot = FIRDatabase.database().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
