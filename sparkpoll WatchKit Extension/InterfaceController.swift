9432d137f24fc757f86f1ae180a3670687f297a7
Alexander Murphy <amurphy345@gmail.com>
2016-09-16 23:29:34 -0600

    Initial Commit

---
+++ sparkpoll WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  sparkpoll WatchKit Extension
+//
+//  Created by Alexander Murphy on 9/16/16.
+//  Copyright © 2016 Alexander Murphy. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
