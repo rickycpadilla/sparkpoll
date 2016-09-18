ae2abf67559b7415d5050298723d48cf4f8ab9e7
Alexander Murphy <amurphy345@gmail.com>
2016-09-17 20:08:52 -0600

    working on storyboard and new lightning poll view controller created

---
+++ sparkpoll/LightningPollControllers/NewLightningPollViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewLightningPollViewController.swift
+//  sparkpoll
+//
+//  Created by Alexander Murphy on 9/17/16.
+//  Copyright © 2016 Alexander Murphy. All rights reserved.
+//
+
+import UIKit
+
+class NewLightningPollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
