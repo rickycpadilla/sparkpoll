623e19130450737354a905b477f41bf0ed896ba3
Alexander Murphy <amurphy345@gmail.com>
2016-09-19 22:53:47 -0600

    out of type conversion hell

--- sparkpoll/JoinLightningPollViewController.swift
+++ sparkpoll/JoinLightningPollViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import MapKit
+import Foundation
 
 class JoinLightningPollViewController: UIViewController, CLLocationManagerDelegate {
     private let locManager = CLLocationManager()
@@ -28,9 +29,9 @@ class JoinLightningPollViewController: UIViewController, CLLocationManagerDelega
         
         if locManager.location?.coordinate != nil {
             //show user location
-            let location:CLLocationCoordinate2D = locManager.location!.coordinate
-            self.userLatitude = Float64(location.latitude)
-            self.userLongitude = Float64(location.longitude)
+            let userLocation:CLLocationCoordinate2D = locManager.location!.coordinate
+            self.userLatitude = Float64(userLocation.latitude)
+            self.userLongitude = Float64(userLocation.longitude)
             print(locManager.location)
         }
         //end location
@@ -46,15 +47,29 @@ class JoinLightningPollViewController: UIViewController, CLLocationManagerDelega
             
             //loop through polls, lets figure out how far away they are
             for (poll_id, poll) in currentPollData {
-                // calculate distance between current location and poll coordinates.
+                //get long and lat from firebase
                 
+                if let unwrapped = poll.value(forKey: "origin_lat") {
+                    let unwrappedLatStr = String(describing: unwrapped)
+                    let pollLatDouble = Double(unwrappedLatStr)
+                }
+                if let unwrappedLng = poll.value(forKey: "origin_lng") {
+                    let unwrappedLngStr = String(describing: unwrappedLng)
+                    let pollLngDouble = Double(unwrappedLngStr)
+                }
+//                print(pollLat, pollLng)
+                
+                //create location from points in firebase
+//                let pollOriginLocation = CLLocation(latitude: poll.value(forKey: "origin_lat"), longitude: poll.value(forKey: "origin_lng"))
+                // calculate distance between current location and poll coordinates.
+//                DistanceCalculationHelper.calculateDistanceBetweenTwoPoints(point_1: locManager.location, point_2: pollOriginLocation)
                 
                 poll.setValue("woot", forKey: "test")
-                //                print("\(poll_id): \(poll.value(forKey: "origin_lat"))")
+//                print("\(poll_id): \(poll.value(forKey: "origin_lat"))")
             }
             
             //            print(currentPollData)
-            print("current coordinates here", self.userLatitude, self.userLongitude )
+//            print("current coordinates here", self.userLatitude, self.userLongitude )
             print("polls here", currentPollData)
         })
     }
